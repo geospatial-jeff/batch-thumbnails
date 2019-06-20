2ff09b9678b7831b69fc8039e2d8f54852760811
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-20 10:14:42 -0700

    preventing duplicate job submissions

--- lambda/handler.py
+++ lambda/handler.py
@@ -6,18 +6,31 @@ batch_client = boto3.client('batch')
 JOB_DEFINITION = os.getenv('JOB_DEFINITION')
 JOB_QUEUE = os.getenv('JOB_QUEUE')
 
-def handler(event, context):
+def kickoff(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
-    args = {
-        'jobName': os.path.splitext(os.path.split(key)[-1]),
-        'jobQueue': JOB_QUEUE,
-        'jobDefinition': JOB_DEFINITION,
-        'parameters': {
-            'in_bucket': bucket,
-            'in_key': key
-        }
-    }
+    job_name = os.path.splitext(os.path.split(key)[-1])[0]
 
-    batch_client.submit_job(**args)
+    # s3:ObjectCreated:* event source can fire two invocations for each upload
+    # Make sure the job id isn't already in batch job queue
+    job_status = ["SUBMITTED", "PENDING", "RUNNABLE", "STARTING", "RUNNING"]
+    job_names = []
+    for job in job_status:
+        current_jobs = batch_client.list_jobs(jobQueue=JOB_QUEUE, jobStatus=job)
+        list(map(lambda x: job_names.append(x['jobName']), current_jobs['jobSummaryList']))
+
+    if job_name not in job_names:
+        args = {
+            'jobName': os.path.splitext(os.path.split(key)[-1])[0],
+            'jobQueue': JOB_QUEUE,
+            'jobDefinition': JOB_DEFINITION,
+            'parameters': {
+                'in_bucket': bucket,
+                'in_key': key
+            }
+        }
+
+        batch_client.submit_job(**args)
+    else:
+        print(f"Job {job_name} was already submitted.")
