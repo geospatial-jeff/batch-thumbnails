7382863b88081adde10dbb183134390251922e6a
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-19 22:47:12 -0700

    lambda function to submit batch job on s3:ObjectCreated event

---
+++ lambda/handler.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+import boto3
+
+s3_res = boto3.resource('s3')
+batch_client = boto3.client('batch')
+
+JOB_DEFINITION = os.getenv('JOB_DEFINITION')
+JOB_QUEUE = os.getenv('JOB_QUEUE')
+
+def handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    args = {
+        'jobName': os.path.splitext(os.path.split(key)[-1]),
+        'jobQueue': JOB_QUEUE,
+        'jobDefinition': JOB_DEFINITION,
+        'parameters': {
+            'in_bucket': bucket,
+            'in_key': key
+        }
+    }
+
+    batch_client.submit_job(**args)
